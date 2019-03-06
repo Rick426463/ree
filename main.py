a663de092f8e88aa1d46d497041a5e43b3aee872
Rick426463 <40384604+Rick426463@users.noreply.github.com>
2019-03-06 23:48:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import random
+import os
+
+client = commands.Bot(command_prefix='??thebot678593083249473892')
+
+@client.event
+async def on_ready():
+  print(f'[{client.user.name} is online]')
+
+@client.event
+async def on_message(message):
+  channel = client.get_channel('552913888169951272')
+  if message.attachments:
+    for x in message.attachments:
+      name = x['filename']
+      url = x['url']
+
+    embed = discord.Embed(title=f'{name}',colour=discord.Colour(random.randint(0x000000,0xFFFFFF)))
+    embed.set_image(url=url)
+    await client.send_message(channel, embed=embed)
+
+client.run(os.getenv('token'),bot=False)
